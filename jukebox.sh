2a1ba5b5a28f013ebf7c46e36c2666585640ae68
coissac <coissac@users.noreply.github.com>
2020-03-23 14:26:36 +0100

    Update jukebox.sh
    Add a bash function to edit config.txt

--- jukebox.sh
+++ jukebox.sh
@@ -11,11 +11,45 @@
 
 ACTIVER_SSH_SERVEUR=1
 
+GPU_MEMORY=300
+
+decode_MPG2
+decode_WVC1
+
+
 ###########################################################################
 #####                                                                 #####
 ##### NE PAS EDIITER CI-DESSOUS SAUF SI VOUS SAVEZ CE QUE VOUS FAITES #####
 #####                                                                 #####
 ###########################################################################
+
+####
+#
+# fonctions utilitaires
+#
+
+function edit_config() {
+  local parametre=$1
+  shift
+  local value=$*
+
+  awk -v date="$(date '+%d/%m/%Y at %k:%M')" \
+          '/^ *'${parametre}' *=.*$/ {print; \
+                                      print "# commented out by Jukebox.sh on",date; \
+                                      printf("# ")} \
+           {print $0}' /boot/config.txt \
+    | awk -v date="$(date '+%d/%m/%Y at %k:%M')" \
+          -v param="${parametre}" \
+          -v value="${value}" \
+          '{print $0} \
+           END {print "#"; \
+                print "# Edited on",date,"by Jukebox.sh"; \
+                print "#"; \
+                print param"="value; \
+                print }'
+}
+
+
 ####
 #
 # Remise à jour du système
@@ -31,3 +65,11 @@ yes Y | sudo apt-get dist-upgrade
 if (( ACTIVER_SSH_SERVEUR == 1 )) ; then
   sudo systemctl enable ssh
 fi  
+
+####
+#
+# Modification du fichier /boot/config.txt
+#
+#   - Ajouter la part de la mémoire dédiée au GPU
+#   - Ajouter les eventuelles licences pour les 
+#     accellerations materielles des codecs video 
